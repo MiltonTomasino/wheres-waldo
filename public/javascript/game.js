6f6bd735fba352f489e59db7f9666f8751bc9180
MiltonTomasino <jonathantomasino13@gmail.com>
2025-09-19 23:39:27 -0700

    Add Visual Feedback to Click and Meny Options
    Red circle appears on click to let user know what
    is being clicked. Also, the choice is locked in
    when clicking on one of the menu options after the
    first initial click.

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -1,5 +1,7 @@
 const imageContainer = document.querySelector(".image-container");
 const gameStart = document.querySelector(".start-game");
+const menu = document.querySelector(".options-menu");
+const circle = document.querySelector(".selection-circle");
 
 const waldoCoordinates = {
     x1: 1859 / 2560,
@@ -9,20 +11,47 @@ const waldoCoordinates = {
 }
 
 const file = JSON.parse(imageContainer.dataset.file);
+let pctX = 0;
+let pctY = 0;
 
 async function getCoordinates(event) {
     const rect = event.target.getBoundingClientRect();
-    const clickX = event.clientX - rect.left;
-    const clickY = event.clientY - rect.top;
+    const x = event.clientX;
+    const y = event.clientY;
+    const clickX = x- rect.left;
+    const clickY = y - rect.top;
 
-    const pctX = clickX / rect.width;
-    const pctY = clickY / rect.height;
+    pctX = clickX / rect.width;
+    pctY = clickY / rect.height;
 
-    console.log(`Mouse click coordinates: X(${clickX}), Y(${clickY})`);
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+    menu.style.left = `${x + 40}px`;
+    menu.style.top = `${y}px`;
+    menu.style.display = "block";
+
+    circle.style.display = "block";
+
+    console.log(`Mouse click coordinates: X(${x}), Y(${y})`);
+}
+
+gameStart.addEventListener("click", async (e) => {
+    e.preventDefault();
+    await fetch("/game/start", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"}
+        });
+    imageContainer.style.display = "block";
+})
+
+menu.addEventListener("click", async (e) => {
+    e.stopPropagation();
+    if (!e.target.dataset.choice) return;
 
     if (
         pctX >= file.coords.x1 && pctX <= file.coords.x2 &&
         pctY >= file.coords.y1 && pctY <= file.coords.y2
+        && e.target.dataset.choice === "waldo"
     ) {
         console.log("You found Waldo!");
         const end = await fetch("/game/end", {
@@ -33,13 +62,5 @@ async function getCoordinates(event) {
     } else {
         console.log("Try again!");
     }
-}
 
-gameStart.addEventListener("click", async (e) => {
-    e.preventDefault();
-    await fetch("/game/start", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"}
-        });
-    imageContainer.style.display = "block";
 })
