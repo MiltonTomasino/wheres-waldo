5a145c0ea1d9a9d4ad9669017f1e69ddaa160511
MiltonTomasino <jonathantomasino13@gmail.com>
2025-09-27 13:57:27 -0700

    Add Second Image to Game

--- prisma/seed.js
+++ prisma/seed.js
@@ -17,28 +17,57 @@ async function main() {
     //     }
     // });
 
-    await prisma.fileInfo.update({
-        where: {
-            name: "Waldo"
-        },
+    // await prisma.fileInfo.update({
+    //     where: {
+    //         name: "Waldo"
+    //     },
+    //     data: {
+    //         coords: [
+    //             {
+    //                 name: "waldo",
+    //                 axis: {
+    //                     x1: 1859 / 2560,
+    //                     x2: 1917 / 2560,
+    //                     y1: 723 / 1440,
+    //                     y2: 790 / 1440
+    //                 }
+    //             },
+    //             {
+    //                 name: "cannon",
+    //                 axis: {
+    //                     x1: 197 / 2560,
+    //                     x2: 353 / 2560,
+    //                     y1: 1343 / 1440,
+    //                     y2: 1437 / 1440
+    //                 }
+    //             }
+    //         ]
+    //     }
+    // });
+
+    await prisma.fileInfo.create({
         data: {
+            name: "Waldo2",
+            path: "/images/waldo2.jpeg",
+            width: 2800,
+            height: 1760,
             coords: [
                 {
                     name: "waldo",
                     axis: {
-                        x1: 1859 / 2560,
-                        x2: 1917 / 2560,
-                        y1: 723 / 1440,
-                        y2: 790 / 1440
+                        x1: 1143 / 2800,
+                        x2: 1223 / 2800,
+                        y1: 285 / 1760,
+                        y2: 382 / 1760
                     }
                 },
                 {
-                    name: "cannon",
+                    name: "pipe smoker",
                     axis: {
-                        x1: 197 / 2560,
-                        x2: 353 / 2560,
-                        y1: 1343 / 1440,
-                        y2: 1437 / 1440
+                        x1: 1417 / 2800,
+                        x2: 1499 / 2800,
+                        y1: 1456 / 1760,
+                        y2: 1630 / 1760
                     }
                 }
             ]
